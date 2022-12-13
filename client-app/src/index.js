0f72b61a1b1444b1dfbc92523f7b67321e590180
DatNgo <ngominhdat110115@gmail.com>
2022-12-13 13:59:04 +0700

    init firebase

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -5,6 +5,28 @@ import { Provider } from "react-redux";
 import store from './store'
 import { BrowserRouter } from 'react-router-dom';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB3YpBEg2acCbZac8VK--C4q2Pj7bYRfQY",
+    authDomain: "e-working-netcore.firebaseapp.com",
+    projectId: "e-working-netcore",
+    storageBucket: "e-working-netcore.appspot.com",
+    messagingSenderId: "805851943445",
+    appId: "1:805851943445:web:14de78ac62038354bd341c",
+    measurementId: "G-EXC8H9ZFXT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
